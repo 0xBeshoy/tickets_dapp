4126fead9c2e9b7fb82696f18eacee8abd9e3a5d
Beshoy Shehata <0xbeshoy@gmail.com>
2022-02-04 01:06:18 +0200

    some progress

--- contracts/Tickets.sol
+++ contracts/Tickets.sol
@@ -18,25 +18,31 @@ contract Tickets is Ownable {
         uint256 Evt_End_Date; // EPOCH Date
         bool Evt_Approved; // Whether if the event has been approved by the admin or not
         bool Evt_Tkt_Sale_Ready; // Whether if the event tickets is available for sale right now or not
+        TicketDetails[] Evt_Tickets; // The Event tickets struct array which will contain every ticket purchased for that specific event
     }
 
     struct TicketDetails {
         uint256 Tkt_ID; // Generated Ticket ID
         uint256 Evt_ID; // Linked Event ID
+        address Tkt_Owner; // Owner of this specific ticket
         uint256 Tkt_Price; // Purchased ticket price
         uint256 Tkt_Purchase_Date; //EPOCH date of ticket purchase date
         uint256 Evt_Start_Date; // EPOCH Date
         uint256 Evt_End_Date; // EPOCH Date
     }
 
-    address[] internal allEventManagers; // All event managers in the contract
-    address[] internal allTicketBuyers; // All buyers of all tickets in the contract
+    address[] public allEventManagers; // All event managers in the contract
+    address[] public allTicketBuyers; // All buyers of all tickets in the contract
     address[] public allEventTicketBuyers; // All buyers of tickets in specific event
-    EventDetails[] public allEvents; // All events in an array;
+    uint256[] public allEvents; // All event IDs in one array;
+    // uint256[][] public allTickets; // All ticket IDs in one array
+    EventDetails[] public allEventStructs; // All event structs in one array
 
-    mapping(uint256 => EventDetails) public eventDB; // All events
-    mapping(uint256 => uint256[]) public eventTickets; // All tickets in one event (<eventID> => ticketsArray[])
+    mapping(uint256 => EventDetails) public eventsDB; // All events
+    mapping(uint256 => uint256[]) public eventTickets; // All tickets in a specific event (<eventID> => ticketsArray[])
     mapping(uint256 => address) public ownerOfTicket; // An owner of a specific ticket (<ticketID> => address)
+    mapping(uint256 => mapping(address => uint256))
+        private balanceOfEthPerOwner; // All eth paid by a specific owner in a specific event. Should default to ZERO per event after event is disabled or ended FIXME: this is a bug which may allow balance to be either wiped out or be easily stolen
     mapping(uint256 => mapping(address => uint256)) balanceOfTickets; // Tickets amount per address per event <eventID => (buyerAddress => balance)> // could we use ticketsOfAddress[eventID][address][balance].length?
     mapping(uint256 => mapping(address => uint256[])) public ticketsOfAddress; // All tickets owned by an address per event (<eventID> => (buyerAddress => ticketsOwnedArray[]))
     mapping(address => uint256[]) public eventsPerAddress; // All events attended by specific address (<address> => eventsArray[])
@@ -44,40 +50,175 @@ contract Tickets is Ownable {
     mapping(address => uint256[]) public eventsPerManager; // All events per manager address
 
     // Modifiers
-    // modifier isEventManager(uint256 _eventID) {
+    modifier isEventManager(uint256 eventID_) {
+        require(
+            eventsDB[eventID_].Evt_Mgr == msg.sender,
+            "Only Event Managers are allowed to do this action"
+        );
+        _;
+    }
+
+    // modifier isTicketBuyer{uint eventID_){
     //     require(
-    //         eventDB[_eventID].Evt_Mgr == msg.sender,
-    //         "Only Event Managers are allowed to do this action"
+
     //     );
     //     _;
     // }
 
-    // modifier isTicketBuyer{uint _eventID){
-    //     require(
-
-    //     );
-    //     _;
-    // }
+    modifier isSaleReady(uint256 eventID_) {
+        require(
+            eventsDB[eventID_].Evt_Tkt_Sale_Ready == true,
+            "Event ticket sales should be approved first"
+        );
+        _;
+    }
 
     // Some utility functions
 
-    /// @notice Returns a pseudo-random number
-    /// @dev Will be used to generate EventID, TicketsID, etc.,
-    function getRandNum() public view returns (uint8) {
-        uint256 _rand = (uint256(block.gaslimit) +
-            uint256(block.number) +
-            uint256(block.difficulty) +
-            uint256(block.timestamp) +
-            uint256(NONCE));
-
-        return (uint8(_rand));
+    constructor() {
+        NONCE = uint256(
+            keccak256(
+                abi.encodePacked(
+                    block.number,
+                    block.difficulty,
+                    block.timestamp,
+                    msg.sender
+                )
+            )
+        );
     }
 
-    // function createEvent() isEventManager {}
+    /// @notice Returns a pseudo-random number
+    /// @dev Will be used to generate EventID, TicketsID, etc.,
+    function generateRandNum() public view returns (uint256) {
+        uint256 randomNumber = uint256(
+            keccak256(
+                abi.encodePacked(
+                    block.number,
+                    block.difficulty,
+                    block.timestamp,
+                    msg.sender,
+                    NONCE
+                )
+            )
+        );
+        return (randomNumber % (1000000 - 100000)) + 100000;
+    }
 
-    // function disableEvent() onlyOwner {}
+    function createEvent(
+        uint256 price_,
+        uint256 amount_,
+        uint256 startDate_,
+        uint256 endDate_
+    ) public payable returns (uint256) {
+        uint256 _eventID = generateRandNum();
+        eventsDB[_eventID].Evt_ID = _eventID;
+        eventsDB[_eventID].Evt_Mgr = msg.sender;
+        eventsDB[_eventID].Evt_Tkt_Price = price_;
+        eventsDB[_eventID].Evt_Tkt_Allowed_Count = amount_;
+        eventsDB[_eventID].Evt_Start_Date = startDate_;
+        eventsDB[_eventID].Evt_End_Date = endDate_;
+        eventsDB[_eventID].Evt_Approved = false;
+        eventsDB[_eventID].Evt_Tkt_Sale_Ready = false;
 
-    // function buyTickets() {}
+        allEvents.push(_eventID);
+        return _eventID;
+    }
+
+    /// @notice Allows the Contract admin to approve the Event
+    /// @dev onlyOwner modifier should be invoked
+    /// @param eventID_ EventID returned from the createEvent() function
+    /// @return bool true/false if the approval was successful of not
+    function approveEvent(uint256 eventID_) public onlyOwner returns (bool) {
+        require(eventsDB[eventID_].Evt_ID > 0, "Event Not Found");
+        require(
+            eventsDB[eventID_].Evt_Approved == false,
+            "Event already approved before"
+        );
+        eventsDB[eventID_].Evt_Approved = true;
+
+        return true;
+    }
+
+    /// @notice Allows the event manager to approve the selling of the event tickets
+    /// @dev isEventManager modifier should be invoked
+    /// @param eventID_ EventID returned from the createEvent() function
+    /// @return bool true/false if the approval was successful of not
+    function allowSale(uint256 eventID_)
+        public
+        isEventManager(eventID_)
+        returns (bool)
+    {
+        require(eventsDB[eventID_].Evt_ID > 0, "Event Not Found");
+        require(
+            eventsDB[eventID_].Evt_Approved == true,
+            "Event should be approved first by the contract admin"
+        );
+        require(
+            eventsDB[eventID_].Evt_Tkt_Sale_Ready == false,
+            "Event ticket sales already approved by an event manager"
+        );
+
+        require(
+            eventsDB[eventID_].Evt_End_Date > block.timestamp,
+            "Event has already been ended"
+        );
+
+        eventsDB[eventID_].Evt_Tkt_Sale_Ready = true;
+
+        return true;
+    }
+
+    function disableEvent(uint256 eventID_) public onlyOwner returns (bool) {
+        require(
+            eventsDB[eventID_].Evt_Approved == true &&
+                eventsDB[eventID_].Evt_End_Date > block.timestamp,
+            "Event isn't approved already and/or the event has already been ended"
+        );
+
+        eventsDB[eventID_].Evt_Approved = false;
+
+        return true;
+    }
+
+    function buyTickets(uint256 eventID_, uint256 amount_)
+        public
+        payable
+        isSaleReady(eventID_)
+    {
+        require(
+            eventsDB[eventID_].Evt_Start_Date < block.timestamp &&
+                eventsDB[eventID_].Evt_End_Date > block.timestamp,
+            "Event should be started first and/or shouldn't be ended too"
+        );
+        require(
+            amount_ > 0 &&
+                eventsDB[eventID_].Evt_Tkt_Allowed_Count >= // Number of max allowed tickets to be sold
+                (eventTickets[eventID_].length + amount_), // Number of already purchased tickets
+            "You must supply an amount of tickets that is less than the max allowed tickets to be ever sold"
+        );
+        uint256 totalPrice = eventsDB[eventID_].Evt_Tkt_Price * amount_;
+        require(msg.value >= totalPrice);
+
+        for (uint256 i; i <= amount_; i++) {
+            uint256 tk_id = eventTickets[eventID_].length + 1;
+            eventsDB[eventID_].Evt_Tickets.push();
+            eventsDB[eventID_].Evt_Tickets[i].Tkt_ID = tk_id;
+            eventsDB[eventID_].Evt_Tickets[i].Evt_ID = eventID_;
+            eventsDB[eventID_].Evt_Tickets[i].Tkt_Owner = msg.sender;
+            eventsDB[eventID_].Evt_Tickets[i].Tkt_Price = msg.value / amount_;
+            eventsDB[eventID_].Evt_Tickets[i].Tkt_Purchase_Date = block
+                .timestamp;
+            eventsDB[eventID_].Evt_Tickets[i].Evt_Start_Date = eventsDB[
+                eventID_
+            ].Evt_Start_Date;
+            eventsDB[eventID_].Evt_Tickets[i].Evt_End_Date = eventsDB[eventID_]
+                .Evt_End_Date;
+
+            eventTickets[eventID_][i] = tk_id;
+            ownerOfTicket[tk_id] = msg.sender;
+        }
+    }
 
     // function requestRefund() isAttendee {}
 
